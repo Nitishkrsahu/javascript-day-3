54412bb0ae9ef5cb34eed98b7975de3684d00a7c
Nitish <nitishkrsahu123@gmail.com>
2025-04-16 23:23:03 +0530

    some changes in js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ function onloadpage(){
     document.getElementById("btn").value = "Register";
 }
 function btnfun(){
-    var x = confirm("Record will Deleted \nAre you sure ?.");
-    if(true == x){
-        document.querySelector("p").innerHTML = "Record Deleted successfully!!"
-    }
-    else{
-        document.querySelector("p").innerHTML = "You Canceled!!";
-    }
+   var x = prompt("Enter any word.");
+   if(x == ""){
+    document.querySelector("p").innerHTML = "Please Enter Any Input !";
+   }
+   else if(x == null){
+    document.querySelector("p").innerHTML = "You Cancel!"
+   }
+   else{
+    document.querySelector("p")
+   }
 }
