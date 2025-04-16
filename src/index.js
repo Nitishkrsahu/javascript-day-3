7b9b8dce8464456d00324514e550302f587cebe4
Nitish <nitishkrsahu123@gmail.com>
2025-04-16 19:02:45 +0530

    output using  innerHTML

---
+++ src/index.js
@@ -0,0 +1,7 @@
+"use strict"
+function onloadpage(){
+    document.getElementById("btn").value = "Register";
+}
+function btnfun(){
+    document.querySelector("p").innerHTML = "<b>Record is Deleted</b>!..";
+}
